b8cca08cd78fee5eb7ae9839d6880038eb7bbf5c
vaughn-johnson <vneck@uw.edu>
2020-11-07 11:03:40 -0800

    updated copy and styling of introduction

--- src/pages/home/Body/Body.js
+++ src/pages/home/Body/Body.js
@@ -11,12 +11,19 @@ const Body = () => (
     }}
   >
     <div style={bodyText}>
-      &nbsp; &nbsp; &nbsp; Hi! I am a software developer and a trained
-      statistician. I have years of experience working closely with a product
-      focused team. I value simplicity, legibility, and sustainability, and I
-      embody those values in my work. My passion is helping people make
-      decisions based on evidence.
+      <p>
+        &nbsp; &nbsp; &nbsp; &nbsp;Hi! I am a software developer and a trained
+        statistician. I have years of experience working on a product
+        obsessed team, and I have a background in linguistics and physics.
+      </p>
+
+      <p>
+        &nbsp; &nbsp; &nbsp; &nbsp;I value simplicity, legibility, and maintainability, and I
+        embody those values in my work. My passion is helping people make
+        decisions based on evidence. 
+      </p>
     </div>
+
     <img
       style={{ height: "100%", width: "100%" }}
       src={SIGNATURE_URL}
